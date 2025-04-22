153366bb29d3eb565d4c737202bc6f47b9d3bee3
Janetmaria <janetsaji1652@gmail.com>
2025-04-22 10:38:35 +0000

    Updated production

--- todo-app/app.js
+++ todo-app/app.js
@@ -4,6 +4,7 @@ const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 const path =  require("path");
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
